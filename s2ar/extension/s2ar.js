2b668dc8fa38cada616bb21bbe4db253bea7f257
creativival <creativival@gmail.com>
2018-10-21 21:56:09 +0900

    update transparency

--- s2ar/extension/s2ar.js
+++ s2ar/extension/s2ar.js
@@ -4,11 +4,16 @@
       var socket;
       var hostname = "s2ar-helper.glitch.me";
       var roomId;
+      var connected = false;
 
       ext._shutdown = function() {};
 
       ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        if (connected) {
+          return {status: 2, msg: 'Connected to the Server'};
+        } else {
+          return {status: 1, msg: 'Not connected to the Server'};
+        }
       };
 
       ext.set_hostname = function(str) {
@@ -19,6 +24,7 @@
         roomId = str1 + "-" + str2;
         socket = io.connect('http://' + hostname);
         socket.on("connect", function() {
+          connected = true;
           socket.emit("from_client", JSON.stringify({roomId: roomId, command: "connect"}));
         });
       }
@@ -98,6 +104,11 @@
         socket.emit("from_client", JSON.stringify({roomId: roomId, command: command}));
       }
 
+      ext.set_alpha = function(a) {
+        let command = "set_alpha:" + a;
+        socket.emit("from_client", JSON.stringify({roomId: roomId, command: command}));
+      }
+
       ext.remove_cube = function(x, y, z) {
         let command = "remove_cube:" + x + ":" + y + ":" + z;
         socket.emit("from_client", JSON.stringify({roomId: roomId, command: command}));
@@ -116,18 +127,19 @@
           change_cube_size: 'ブロックサイズの変更。拡大倍率を %n',
           set_cube: 'ブロックを置く。x座標を %n 、y座標を %n 、z座標を %n',
           set_box: '直方体を置く。x座標を %n 、y座標を %n 、z座標を %n 、幅を %n 、奥行を %n 、高さを %n',
-          set_cylinder: '円柱を置く。x座標を %n 、y座標を %n 、z座標を %n 、半径を %n 、高さを %n 、 %s 軸',
-          set_hexagon: '六角柱を置く。x座標を %n 、y座標を %n 、z座標を %n 、半径を %n 、高さを %n 、 %s 軸',
+          set_cylinder: '円柱を置く。x座標を %n 、y座標を %n 、z座標を %n 、半径を %n 、高さを %n 、 %m.axes 軸',
+          set_hexagon: '六角柱を置く。x座標を %n 、y座標を %n 、z座標を %n 、半径を %n 、高さを %n 、 %m.axes 軸',
           set_sphere: '球を置く。x座標を %n 、y座標を %n 、z座標を %n 、半径を %n',
-          set_char: '文字を書く。x座標を %n 、y座標を %n 、z座標を %n 、文字 %s 、 %s 軸',
+          set_char: '文字を書く。x座標を %n 、y座標を %n 、z座標を %n 、文字 %s 、 %m.axes 軸',
           set_line: '２点間に線を引く。x1 を %n 、y1 を %n 、z1 を %n 、x2 を %n 、y2 を %n 、z2を %n',
-          set_roof: '屋根を作る。x座標を %n 、y座標を %n 、z座標を %n 、幅を %n 、奥行を %n 、高さを %n 、 %s 軸に',
+          set_roof: '屋根を作る。x座標を %n 、y座標を %n 、z座標を %n 、幅を %n 、奥行を %n 、高さを %n 、 %m.axes 軸',
           polygon_file_format: '3Dモデルを作成。x座標を %n 、y座標を %n 、z座標を %n 、PLYファイル %s',
           animation: 'アニメーション。x座標を %n 、y座標を %n 、z座標を %n 、差分Xを %n 、 差分Yを %n 、 差分Zを %n 、 時間を %n 、回数を %n 、モデルデータを %s',
-          map: '地図を作成。地図データを %s 、横を %n 、縦を %n 、拡大倍率を %n 、（低地の色を r1: %n g1: %n b1: %n ）、（高地の色を r2: %n g2: %n b2: %n ）、上方向へ %n',
-          pin: 'ピンを立てる。位置データを %s 、横を %n 、縦を %n 、拡大倍率を %n 、左上緯度を %n 、左上経度を %n 、右下緯度を %n 、右下経度を %n 横に %n',
+          map: '地図を作成。地図データを %s 、幅を %n 、高さを %n 、拡大倍率を %n 、（低地の色を r1: %n g1: %n b1: %n ）、（高地の色を r2: %n g2: %n b2: %n ）、上方向へ %n',
+          pin: 'ピンを立てる。位置データを %s 、幅を %n 、高さを %n 、拡大倍率を %n 、左上緯度を %n 、左上経度を %n 、右下緯度を %n 、右下経度を %n 横に %n ずらす',
           molecular_structure: '分子構造モデルを作成。x座標を %n 、y座標を %n 、z座標を %n 、拡大倍率を %n 、MLDファイル %s',
           set_color: 'ブロックの色を変える。r: %n g: %n b: %n',
+          set_alpha: 'ブロックの透明度を変える。alpha: %n',
           remove_cube: 'ブロックを消す。x座標を %n 、y座標を %n 、z座標を %n',
           reset: 'リセット'
         },
@@ -137,18 +149,19 @@
           change_cube_size: 'change cube size maginification: %n',
           set_cube: 'set cube at x: %n y: %n z: %n',
           set_box: 'set box at x: %n y: %n z: %n wide: %n depth: %n height: %n',
-          set_cylinder: 'set cylinder at x: %n y: %n z: %n radius: %n height: %n axis: %s',
-          set_hexagon: 'set hexagon at x: %n y: %n z: %n radius: %n height: %n axis: %s',
+          set_cylinder: 'set cylinder at x: %n y: %n z: %n radius: %n height: %n axis: %m.axes',
+          set_hexagon: 'set hexagon at x: %n y: %n z: %n radius: %n height: %n axis: %m.axes',
           set_sphere: 'set sphere at x: %n y: %n z: %n radius: %n',
-          set_char: 'set char at x: %n y: %n z: %n letter: %s axis: %s',
+          set_char: 'set char at x: %n y: %n z: %n letter: %s axis: %m.axes',
           set_line: 'set line between x1: %n y1: %n z1: %n and x2: %n y2: %n z2: %n',
-          set_roof: 'set roof at x: %n y: %n z: %n wide: %n depth: %n height: %n axis: %s',
+          set_roof: 'set roof at x: %n y: %n z: %n wide: %n depth: %n height: %n axis: %m.axes',
           polygon_file_format: 'create 3d model at x: %n y: %n z: %n ply file: %s',
           animation: 'animation at x: %n y: %n z: %n diffX: %n diffY: %n diffZ: %n time: %n times: %n models: %s',
           map: 'draw map from csv: %s width: %n height: %n magnification: %n (lowland r1: %n g1: %n b1: %n ) (highland r2: %n g2: %n b2: %n ) upward: %n',
-          pin: 'stand pins at potision: %s width: %n height: %n magnification: %n up-left (latitude: %n longitude: %n ) down-right (latitude: %n longitude: %n ) shift %n',
+          pin: 'stand pins at position: %s width: %n height: %n magnification: %n up-left (latitude: %n longitude: %n ) down-right (latitude: %n longitude: %n ) shift %n',
           molecular_structure: 'molecular structure at x: %n y: %n z: %n magnification: %n mld file: %s',
           set_color: 'set color to r: %n g: %n b: %n',
+          set_alpha: 'set transparency to alpha: %n',
           remove_cube: 'remove cube at x: %n y: %n z: %n',
           reset: 'reset'
         },
@@ -173,9 +186,13 @@
           [' ', locale[lang].pin, 'pin', 'potision_data.csv', 257, 257, 2, 46.852, 126.738, 29.148, 149.238, 0],
           [' ', locale[lang].molecular_structure, 'molecular_structure', 0, 10, 0, 10, 'methane.mld'],
           [' ', locale[lang].set_color, 'set_color', 255, 255, 255],
+          [' ', locale[lang].set_alpha, 'set_alpha', 1.0],
           [' ', locale[lang].remove_cube, 'remove_cube', 1, 0, 1],
           [' ', locale[lang].reset, 'reset']
-        ]
+        ],
+        menus: {
+          axes: ['x', 'y', 'z']
+        }
       };
 
       ScratchExtensions.register('S2AR(Scratch2ARKit)', descriptor, ext);
